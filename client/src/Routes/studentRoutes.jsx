928ebdf6aeb51e2bbf4ddf922d593c867d2ca510
nghaan1810 <ngochan.tv253@gmail.com>
2024-11-04 22:39:11 +0700

    add icon

--- client/src/Routes/studentRoutes.jsx
+++ client/src/Routes/studentRoutes.jsx
@@ -1,26 +1,26 @@
-import Profile from "../views/Studennt/Profile";
+// import
 import Dashboard from "../views/Student/Dashboard";
+import Tables from "../views/Dashboard/Tables";
+import Profile from "../views/Dashboard/Profile";
+import { TbHome } from "react-icons/tb";
 import { IoMdPerson } from "react-icons/io";
-import {
-  HomeIcon,
-} from "../components/Icons/Icons";
-
+import { MdOutlineHistory } from "react-icons/md";
 
 var dashRoutes = [
   {
     path: "/dashboard",
     name: "Trang chủ",
-    icon: <HomeIcon color="inherit" />,
+    icon: <TbHome color="inherit" />,
     component: Dashboard,
     layout: "/student",
   },
-  // {
-  //   path: "/tables",
-  //   name: "Lịch sử mượn/ trả sách",
-  //   icon: <MdOutlineHistory color="inherit" />,
-  //   component: Tables,
-  //   layout: "/student",
-  // },
+  {
+    path: "/tables",
+    name: "Lịch sử mượn/ trả sách",
+    icon: <MdOutlineHistory color="inherit" />,
+    component: Tables,
+    layout: "/student",
+  },
 
   {
     name: "ACCOUNT PAGES",
@@ -30,6 +30,7 @@ var dashRoutes = [
       {
         path: "/profile",
         name: "Profile",
+        rtlName: "لوحة القيادة",
         icon: <IoMdPerson color="inherit" />,
         secondaryNavbar: true,
         component: Profile,
