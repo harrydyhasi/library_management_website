dc5b50b82eef4c06a364bcdafe5fc3b851d42251
duycd-dev <duy.cao@affina.com.vn>
2024-11-24 09:45:02 +0700

    import feature

--- server/routes/bookRoutes.js
+++ server/routes/bookRoutes.js
@@ -1,13 +1,16 @@
 const express = require('express');
 const router = express.Router();
-const { addBook, updateBook, deleteBook, getAllBooks, getBookById, updateQuantityBook } = require('../controllers/bookController');
-const upload = require('../middlewares/uploadImage'); 
+const { addBook, updateBook, deleteBook, getAllBooks, getBookById, updateQuantityBook, importBooks } = require('../controllers/bookController');
+const upload = require('../middlewares/uploadImage');
+const multer = require('multer');
+const uploadCsv = multer({ dest: 'uploads/' });
 
 router.post('/book', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'pdf', maxCount: 1 }]), addBook);
 router.put('/book/:id', upload.fields([{ name: 'image', maxCount: 1 }, { name: 'pdf', maxCount: 1 }]), updateBook);
 router.delete('/book/:id', deleteBook);
 router.get('/book', getAllBooks);
 router.get('/book/:id', getBookById);
-router.put('/book/quantity/:id', updateQuantityBook)
+router.put('/book/quantity/:id', updateQuantityBook);
+router.post('/book/import', uploadCsv.single('file'), importBooks);
 
 module.exports = router;
--- server/routes/categoryRoutes.js
+++ server/routes/categoryRoutes.js
@@ -1,11 +1,15 @@
 const express = require('express');
 const router = express.Router();
-const { addCategory, updateCategory, deleteCategory, getAllCategories, getCategoryById } = require('../controllers/categoryController');
+const { addCategory, updateCategory, deleteCategory, getAllCategories, getCategoryById, importCategories } = require('../controllers/categoryController');
+const multer = require('multer');
+const upload = multer({ dest: 'uploads/' });
+
 
 router.post('/category', addCategory);
 router.put('/category/:id', updateCategory);
 router.delete('/category/:id', deleteCategory);
 router.get('/category', getAllCategories);
 router.get('/category/:id', getCategoryById);
+router.post('/category/import', upload.single('file'), importCategories);
 
 module.exports = router;
